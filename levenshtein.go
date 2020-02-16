8d701c59f154d068fde7c924fb309d9d62e90ee1
Benjamin King <ben@tuemlo.com>
2020-02-16 21:56:33 +0000

    Add Levenshtein recursive implementation
    Type: feat

--- levenshtein.go
+++ levenshtein.go
@@ -5,8 +5,40 @@ import "math"
 type Levenshtein struct {
 }
 
+// TODO Move away from recursive to more efficient.
 func (l Levenshtein) Distance(s1, s2 string) float64 {
+	l1, l2 := len(s1), len(s2)
 
+	// base case: empty strings
+	if l1 == 0 {
+		return float64(l2)
+	}
+	if l2 == 0 {
+		return float64(l1)
+	}
+
+	var cost float64
+	if s1[l1-1] == s2[l2-1] {
+		cost = 0
+	} else {
+		cost = 1
+	}
+	return minimum(
+		l.Distance(s1[:l1-1], s2)+1,
+		l.Distance(s1, s2[:l2-1])+1,
+		l.Distance(s1[:l1-1], s2[:l2-1])+cost,
+	)
+}
+
+func minimum(a, b, c float64) float64 {
+	minimum := a
+	if minimum > b {
+		minimum = b
+	}
+	if minimum > c {
+		return c
+	}
+	return minimum
 }
 
 func (l Levenshtein) Minimum(s1, s2 string) float64 {
@@ -18,5 +50,6 @@ func (l Levenshtein) Minimum(s1, s2 string) float64 {
 }
 
 func (l Levenshtein) Maximum(s1, s2 string) float64 {
-
+	l1, l2 := float64(len(s1)), float64(len(s2))
+	return max(l1, l2)
 }
