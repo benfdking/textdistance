1e36f84d846d6c10431a611c3ddcb0d1bca007a3
Benjamin King <ben@tuemlo.com>
2020-02-21 13:47:40 +0000

    Implement faster levenshtein algorithm
    Type: feat
    
    Issue: #11
    
    - Implemented faster levenshtein algorithm which increases performance
    against master by 1615 to 88 ns

--- levenshtein.go
+++ levenshtein.go
@@ -1,33 +1,64 @@
 package textdistance
 
-import "math"
+import (
+	"math"
+	"unicode/utf8"
+)
 
 type Levenshtein struct {
 }
 
-// TODO Move away from recursive to more efficient.
-func (l Levenshtein) Distance(s1, s2 string) float64 {
-	l1, l2 := len(s1), len(s2)
-
-	// base case: empty strings
-	if l1 == 0 {
-		return float64(l2)
+func (l Levenshtein) Distance(s1, b string) float64 {
+	if len(s1) == 0 {
+		return float64(utf8.RuneCountInString(b))
 	}
-	if l2 == 0 {
-		return float64(l1)
+	if len(b) == 0 {
+		return float64(utf8.RuneCountInString(s1))
 	}
 
-	var cost float64
-	if s1[l1-1] == s2[l2-1] {
-		cost = 0
-	} else {
-		cost = 1
+	if s1 == b {
+		return 0
 	}
-	return minimum(
-		l.Distance(s1[:l1-1], s2)+1,
-		l.Distance(s1, s2[:l2-1])+1,
-		l.Distance(s1[:l1-1], s2[:l2-1])+cost,
-	)
+
+	rs1 := []rune(s1)
+	rs2 := []rune(b)
+
+	// swap to save some memory O(min(a,b)) instead of O(a)
+	if len(rs1) > len(rs2) {
+		rs1, rs2 = rs2, rs1
+	}
+	lenS1 := len(rs1)
+	lenS2 := len(rs2)
+
+	x := make([]int32, lenS1+1)
+	// we start from 1 because index 0 is already 0.
+	for i := int32(1); i < int32(lenS1); i++ {
+		x[i] = i
+	}
+
+	// fill in the rest
+	for i := 1; i <= int(lenS2); i++ {
+		prev := int32(i)
+		var current int32
+		for j := 1; j <= int(lenS1); j++ {
+			if rs2[i-1] == rs1[j-1] {
+				current = x[j-1] // match
+			} else {
+				current = minUint(minUint(x[j-1]+1, prev+1), x[j]+1)
+			}
+			x[j-1] = prev
+			prev = current
+		}
+		x[lenS1] = prev
+	}
+	return float64(x[lenS1])
+}
+
+func minUint(a, b int32) int32 {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func minimum(a, b, c float64) float64 {
